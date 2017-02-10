40d59261deb596e51cd174ecc6a83496a30a8f35
glacials <qhiiyr@gmail.com>
2017-02-09 18:30:50 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## split
+
+A Slack chat bot that meows.
+
+### Running
+
+#### Prerequisites
+- Go 1.7
+
+#### First run
+```sh
+go get github.com/glacials/split
+split <slack oauth token>
+```
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: splitbot slack-bot-token\n")
+		os.Exit(1)
+	}
+
+	ws, id := slackConnect(os.Args[1])
+
+	for {
+		m, err := getMessage(ws)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if m.Type == "message" && strings.HasPrefix(m.Text, "<@"+id+">") {
+			// if so try to parse if
+			// looks good, get the quote and reply with the result
+			go func(m Message) {
+				postMessage(ws, Message{
+					Type:    m.Type,
+					Channel: m.Channel,
+					Text:    "meow",
+				})
+			}(m)
+		}
+	}
+}
